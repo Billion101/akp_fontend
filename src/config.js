3756a0b5e33943f0fdff239633dc007470a24b5d
Billion101 <128774567+Billion101@users.noreply.github.com>
2024-12-11 19:12:00 +0700

    api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,13 @@
-const config = {
-    apiUrl: import.meta.env.VITE_API_URL,
-  };
+// const config = {
+//     apiUrl: import.meta.env.VITE_API_URL,
+//   };
   
-  export default config;
+//   export default config;
+
+
+  // config.js
+const config = {
+  apiUrl: "http://54.254.210.4:3000",
+};
+
+export default config;
